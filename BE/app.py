bed1b5ee43d4190143afa2ad9dd0b498bc8e0dc1
Rohan Deshpande <Rohan.Deshpande@trios.com>
2023-09-22 18:35:13 -0400

    Revert "Added JSON Reading Functionality"
    This reverts commit 140c648bb50e1ea2976707dcb9d60e52e38b02c0.

--- BE/app.py
+++ BE/app.py
@@ -1,7 +1,1 @@
 print( 'Welcome to the BackEnd Code' )
-
-# Code to Read JSON File
-with open('Data\companies.json') as jsonFile:
-    fileData = jsonFile.read()
-    print( 'JSON Data is : \n ')
-    print( fileData )

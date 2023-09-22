b8aad6223f387639486876749b4cd4085007d0fb
Rohan Deshpande <Rohan.Deshpande@trios.com>
2023-09-22 18:41:29 -0400

    Added JSON functionality again

--- BE/app.py
+++ BE/app.py
@@ -1,1 +1,7 @@
 print( 'Welcome to the BackEnd Code' )
+
+# Code to Read JSON File
+with open('Data\companies.json') as jsonFile:
+    fileData = jsonFile.read()
+    print( 'JSON Data is : \n ')
+    print( fileData )
